c676f8b51d2c3c5e7acb07a791c055b2079cf5e1
Vikas Ukani <vikasukani5@gmail.com>
2023-12-28 04:47:08 +0530

    get data from supabase.

--- app/supabase.py
+++ app/supabase.py
@@ -2,6 +2,7 @@ import os
 import requests
 from supabase import create_client, Client
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
@@ -9,5 +10,8 @@ url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
-def callProfileAPI() :
-    return requests.get(os.environ.get('PROFILE_URL')).json()
+
+def callProfileAPI():
+    data = supabase.table("profile").select('*').eq('id', 1).single().execute()
+    response = requests.get(data.data.get('profile')).json()
+    return response
